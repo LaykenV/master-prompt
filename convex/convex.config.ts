a0201e4d459b270b34951b1ad7aa417687d91adf
Layken Varholdt <laykenv@gmail.com>
2025-08-24 16:26:34 -0500

    removed polar

--- convex/convex.config.ts
+++ convex/convex.config.ts
@@ -1,13 +1,11 @@
 import { defineApp } from "convex/server";
 import agent from "@convex-dev/agent/convex.config";
 import workflow from "@convex-dev/workflow/convex.config";
-import polar from "@convex-dev/polar/convex.config";
 
 
 
 const app = defineApp();
 app.use(agent);
 app.use(workflow);
-app.use(polar);
 
 export default app;
